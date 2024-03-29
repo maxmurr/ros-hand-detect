d7d880b8279bfe8a1d9de85bc4db1423312b80f3
Max Murray <78588986+maxmurr@users.noreply.github.com>
2024-03-29 13:45:45 +0000

    Delete build directory

--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/maxmurr/github/ros-hand-detect_ws/src
-# Build directory: /home/maxmurr/github/ros-hand-detect_ws/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
-subdirs("hand_detection")
